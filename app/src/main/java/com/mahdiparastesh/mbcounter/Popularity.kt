090fa3563bfaa88f66752451f54158ee88236905
Mahdi Parastesh <fulcrum1378@gmail.com>
2021-07-12 16:19:35 +0430

    2021.07.12 - 1

--- app/src/main/java/com/mahdiparastesh/mbcounter/Popularity.kt
+++ app/src/main/java/com/mahdiparastesh/mbcounter/Popularity.kt
@@ -7,11 +7,11 @@ import com.anychart.AnyChart
 import com.anychart.chart.common.dataentry.DataEntry
 import com.anychart.chart.common.dataentry.ValueDataEntry
 import com.anychart.data.Set
-import com.anychart.enums.Anchor
-import com.anychart.enums.MarkerType
-import com.anychart.enums.TooltipPositionMode
+import com.anychart.enums.*
 import com.anychart.graphics.vector.Stroke
 import com.mahdiparastesh.mbcounter.databinding.PopularityBinding
+import kotlin.Comparator
+import kotlin.collections.ArrayList
 
 class Popularity : AppCompatActivity() {
     private lateinit var b: PopularityBinding
@@ -27,19 +27,21 @@ class Popularity : AppCompatActivity() {
         if (m.onani.value == null || m.summary.value == null) {
             onBackPressed(); return; }
         val stb = Statistics.sinceTheBeginning(m.onani.value!!)
-        val stars = HashMap<String, List<Float>>()
+        var stars = ArrayList<Star>()
         for (x in m.summary.value!!.scores) {
             if (x.key.startsWith(resources.getString(R.string.unknown))) continue
             val scores = ArrayList<Float>()
             for (month in stb)
                 scores.add(Statistics.calcHistory(x.value, month))
-            stars[x.key] = scores.toList()
+            stars.add(Star(x.key, scores.toTypedArray()))
         }
+        stars.sortWith(Star.Sort(1))
+        stars.sortWith(Star.Sort())
 
         val data = ArrayList<DataEntry>()
         for (s in stb.indices) {
             val thisMonthsScores = ArrayList<Float>()
-            stars.forEach { thisMonthsScores.add(it.value[s]) }
+            stars.forEach { thisMonthsScores.add(it.scores[s]) }
             data.add(Frame(stb[s], thisMonthsScores))
         }
 
@@ -56,21 +58,27 @@ class Popularity : AppCompatActivity() {
             stars.forEach {
                 line(
                     set.mapAs("{ x: 'x', value: 'value${if (ss > 0) "$ss" else ""}' }")
-                ).name(it.key)
-                hovered().markers().enabled(true)
-                hovered().markers()
-                    .type(MarkerType.CIRCLE)
-                    .size(4.0)
-                tooltip()
-                    .position("right")
-                    .anchor(Anchor.LEFT_CENTER)
-                    .offsetX(5.0)
-                    .offsetY(5.0)
+                ).name(it.name)
+                hovered().markers().apply {
+                    enabled(true)
+                    type(MarkerType.CIRCLE)
+                    size(4.0)
+                }
+                tooltip().apply {
+                    position("right")
+                    anchor(Anchor.LEFT_CENTER)
+                    offsetX(5.0)
+                    offsetY(5.0)
+                }
                 ss++
             }
-            legend().enabled(true)
-            legend().fontSize(13.0)
-            legend().padding(0.0, 0.0, 10.0, 0.0)
+            legend().apply { // Names of the Stars
+                enabled(true)
+                fontSize(13.0)
+                padding(0.0, 0.0, 11.0, 0.0)
+                position(Orientation.TOP)
+                positionMode(LegendPositionMode.OUTSIDE)
+            }
             b.main.setChart(this)
         }
     }
@@ -79,6 +87,15 @@ class Popularity : AppCompatActivity() {
         ValueDataEntry(x, values[0]) {
         init {
             for (i in 1 until values.size) setValue("value$i", values[i])
+        }
+    }
+
+    private class Star(val name: String, val scores: Array<Float>) {
+        class Sort(val by: Int = 0) : Comparator<Star> {
+            override fun compare(a: Star, b: Star) = when (by) {
+                1 -> a.name.compareTo(b.name)
+                else -> (b.scores.sum() - a.scores.sum()).toInt()
+            }
         }
     }
 }
