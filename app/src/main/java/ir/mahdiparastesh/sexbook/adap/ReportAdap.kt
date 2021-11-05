6819a58711b514b32c8236a0697e2a15d7d34c59
Mahdi Parastesh <fulcrum1378@gmail.com>
2021-11-06 02:20:14 +0330

    2021.11.06 - 2

--- app/src/main/java/ir/mahdiparastesh/sexbook/adap/ReportAdap.kt
+++ app/src/main/java/ir/mahdiparastesh/sexbook/adap/ReportAdap.kt
@@ -47,7 +47,8 @@ class ReportAdap(
         val point = clock.getChildAt(pointPos) as View
         val ampm = l.getChildAt(ampmPos) as TextView
         val date = l.getChildAt(datePos) as TextView
-        val name = l.getChildAt(notesPos) as EditText
+        val name = l.getChildAt(namePos) as EditText
+        val type = l.getChildAt(typePos) as Spinner
 
         // IDs
         clock.id = View.generateViewId()
@@ -56,6 +57,7 @@ class ReportAdap(
         point.id = View.generateViewId()
         date.id = View.generateViewId()
         name.id = View.generateViewId()
+        type.id = View.generateViewId()
 
         // Fix Constraints
         var clockHourLP = clockHour.layoutParams as ConstraintLayout.LayoutParams
@@ -64,20 +66,20 @@ class ReportAdap(
         var clockMinLP = clockMin.layoutParams as ConstraintLayout.LayoutParams
         clockMinLP.bottomToBottom = point.id
         clockMin.layoutParams = clockMinLP
-        var nameLP = name.layoutParams as ConstraintLayout.LayoutParams
-        nameLP.topToTop = clock.id
-        nameLP.bottomToBottom = clock.id
-        name.layoutParams = nameLP
         var dateLP = date.layoutParams as ConstraintLayout.LayoutParams
         dateLP.startToEnd = clock.id
         dateLP.endToStart = name.id
-        dateLP.topToTop = clock.id
-        dateLP.bottomToBottom = clock.id
         date.layoutParams = dateLP
         var ampmLP = ampm.layoutParams as ConstraintLayout.LayoutParams
         ampmLP.startToEnd = clock.id
         ampmLP.bottomToBottom = clock.id
         ampm.layoutParams = ampmLP
+        var nameLP = name.layoutParams as ConstraintLayout.LayoutParams
+        nameLP.topToTop = clock.id
+        nameLP.bottomToBottom = clock.id
+        name.layoutParams = nameLP
+        /*var typeLP = type.layoutParams as ConstraintLayout.LayoutParams
+        type.layoutParams = typeLP*/
 
         // Background
         l.background = GradientDrawable().apply {
@@ -103,6 +105,9 @@ class ReportAdap(
             pivotY = minuteHeight - (pointHeight / 2f)
         }
 
+        // Type
+        type.adapter = TypeAdap()
+
         return MyViewHolder(l)
     }
 
@@ -113,7 +118,8 @@ class ReportAdap(
         //val point = clock.getChildAt(pointPos) as View
         val ampm = h.l.getChildAt(ampmPos) as TextView
         val date = h.l.getChildAt(datePos) as TextView
-        val notes = h.l.getChildAt(notesPos) as EditText
+        val name = h.l.getChildAt(namePos) as EditText
+        val type = h.l.getChildAt(typePos) as Spinner
 
         // Date & Time
         var cal = Calendar.getInstance()
@@ -148,21 +154,31 @@ class ReportAdap(
         ampm.text =
             c.resources.getText(if (cal[Calendar.HOUR_OF_DAY] > 12) R.string.PM else R.string.AM)
 
-        // Notes
-        notes.setText(list[i].name)
-        notes.addTextChangedListener(object : TextWatcher {
+        // Name
+        name.setText(list[i].name)
+        name.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
             override fun afterTextChanged(s: Editable?) {
-                saveET(c, notes, allPos(h, list, allMasturbation), allMasturbation)
+                saveET(c, name, allPos(h, list, allMasturbation), allMasturbation)
             }
         })
-        /*notes.setOnFocusChangeListener { _, b ->
-            if (!b && saveOnBlur && allMasturbation != null) saveET(
-                c, notes, allPos(h, list, allMasturbation), allMasturbation
-            )
-            if (b && scrollOnFocus) handler.obtainMessage(Work.SCROLL, h.l.top - dp(5))
-        }*/
+
+        // Type
+        type.setSelection(list[i].type.toInt(), true)
+        type.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(adapterView: AdapterView<*>?) {}
+            override fun onItemSelected(adapterView: AdapterView<*>?, view: View, i: Int, l: Long) {
+                if (allMasturbation == null) return
+                val pos = allPos(h, list, allMasturbation)
+                if (allMasturbation.size <= pos || pos < 0) return
+                if (allMasturbation[pos].type == i.toByte()) return
+                allMasturbation[pos].type = i.toByte()
+                Work(
+                    c, handler, Work.UPDATE_ONE, listOf(allMasturbation[pos], pos, 1)
+                ).start()
+            }
+        }
 
         // Long Click
         val longClick = View.OnLongClickListener { p0 ->
@@ -187,7 +203,7 @@ class ReportAdap(
         h.l.setOnLongClickListener(longClick)
         clock.setOnLongClickListener(longClick)
         date.setOnLongClickListener(longClick)
-        notes.setOnLongClickListener(longClick)
+        name.setOnLongClickListener(longClick)
     }
 
     override fun getItemCount() = list.size
@@ -237,7 +253,8 @@ class ReportAdap(
         const val pointPos = 2
         const val ampmPos = 1
         const val datePos = 2
-        const val notesPos = 3
+        const val namePos = 3
+        const val typePos = 4
 
         fun compileDate(c: Context, time: Long): String {
             val lm = Calendar.getInstance()
---
+++ app/src/main/java/ir/mahdiparastesh/sexbook/adap/TypeAdap.kt
@@ -0,0 +1,45 @@
+package ir.mahdiparastesh.sexbook.adap
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.view.get
+import ir.mahdiparastesh.sexbook.Fun.Companion.c
+import ir.mahdiparastesh.sexbook.Fun.Companion.pdcf
+import ir.mahdiparastesh.sexbook.R
+
+class TypeAdap : ArrayAdapter<TypeAdap.Type>(c, 0, types) {
+    val inf: LayoutInflater = LayoutInflater.from(context)
+    companion object {
+        val names: Array<String> = c.resources.getStringArray(R.array.types)
+        val types = arrayOf(
+            Type(names[0], R.drawable.wet_dream),
+            Type(names[1], R.drawable.masturbation),
+            Type(names[2], R.drawable.masturbation),
+            Type(names[3], R.drawable.masturbation),
+            Type(names[4], R.drawable.masturbation),
+        )
+    }
+
+    override fun getView(i: Int, convertView: View?, parent: ViewGroup): View =
+        (inf.inflate(R.layout.type_selector, parent, false) as ImageView).apply {
+            setImageResource(types[i].icon)
+            colorFilter = pdcf(c, R.color.mrvNotes)
+        }
+
+    override fun getDropDownView(i: Int, convertView: View?, parent: ViewGroup): View =
+        (inf.inflate(R.layout.type_selector_dd, parent, false) as LinearLayout).apply {
+            val icon = this[0] as ImageView
+            val name = this[1] as TextView
+
+            icon.setImageResource(types[i].icon)
+            icon.colorFilter = pdcf(c, R.color.mrvNotes)
+            name.text = types[i].name
+        }
+
+    data class Type(val name: String, val icon: Int)
+}
