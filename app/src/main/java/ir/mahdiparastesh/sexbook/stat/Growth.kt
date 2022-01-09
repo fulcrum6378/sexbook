33386b2a0f6ad7a5d0658e958e6d21001fa88b23
Mahdi Parastesh <fulcrum1378@gmail.com>
2022-01-09 16:10:26 +0330

    2022.01.09 - 1 (AnyChart -> HelloCharts)

--- app/src/main/java/ir/mahdiparastesh/sexbook/stat/Growth.kt
+++ app/src/main/java/ir/mahdiparastesh/sexbook/stat/Growth.kt
@@ -3,16 +3,12 @@ package ir.mahdiparastesh.sexbook.stat
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import com.anychart.AnyChart
-import com.anychart.chart.common.dataentry.DataEntry
-import com.anychart.enums.*
-import com.anychart.graphics.vector.Stroke
 import ir.mahdiparastesh.sexbook.Fun
 import ir.mahdiparastesh.sexbook.Model
 import ir.mahdiparastesh.sexbook.R
 import ir.mahdiparastesh.sexbook.databinding.GrowthBinding
-import ir.mahdiparastesh.sexbook.stat.Popularity.Frame
 import ir.mahdiparastesh.sexbook.stat.Popularity.Star
+import lecho.lib.hellocharts.model.LineChartData
 
 class Growth : AppCompatActivity() {
     private lateinit var b: GrowthBinding
@@ -32,57 +28,14 @@ class Growth : AppCompatActivity() {
         var stars = ArrayList<Star>()
         for (x in m.summary.value!!.scores) {
             if (Summary.isUnknown(x.key)) continue
-            val scores = ArrayList<Float>()
+            val frames = ArrayList<Star.Frame>()
             for (month in stb)
-                scores.add(Singular.calcHistory(x.value, month, true))
-            stars.add(Star(x.key, scores.toTypedArray()))
+                frames.add(Star.Frame(Singular.calcHistory(x.value, month, true), month))
+            stars.add(Star(x.key, frames.toTypedArray()))
         }
         stars.sortWith(Star.Sort(1))
         stars.sortWith(Star.Sort())
 
-        val data = ArrayList<DataEntry>()
-        for (s in stb.indices) {
-            val thisMonthsScores = ArrayList<Float>()
-            stars.forEach { thisMonthsScores.add(it.scores[s]) }
-            data.add(Frame(stb[s], thisMonthsScores))
-        }
-
-        AnyChart.line().apply {
-            animation(true)
-            crosshair().enabled(true)
-            crosshair()
-                .yLabel(true) // DO: yStroke
-                .yStroke(null as Stroke?, null, null, null as String?, null as String?)
-            tooltip().positionMode(TooltipPositionMode.POINT)
-
-            val set = com.anychart.data.Set.instantiate().apply { data(data) }
-            var ss = 0
-            stars.forEach {
-                line(
-                    set.mapAs("{ x: 'x', value: 'value${if (ss > 0) "$ss" else ""}' }")
-                ).name(it.name)
-                hovered().markers().apply {
-                    enabled(true)
-                    type(MarkerType.CIRCLE)
-                    size(4.0)
-                }
-                tooltip().apply {
-                    position("right")
-                    anchor(Anchor.LEFT_CENTER)
-                    offsetX(5.0)
-                    offsetY(5.0)
-                }
-                ss++
-            }
-            legend().apply { // Names of the Stars
-                enabled(true)
-                fontSize(13.0)
-                padding(0.0, 0.0, 11.0, 0.0)
-                position(Orientation.TOP)
-                positionMode(LegendPositionMode.OUTSIDE)
-            }
-            background(resources.getString(R.string.anyChartBG))
-            b.main.setChart(this)
-        }
+        b.main.lineChartData = LineChartData().setLines(Popularity.LineFactory(stars))
     }
 }
--- app/src/main/java/ir/mahdiparastesh/sexbook/stat/Popularity.kt
+++ app/src/main/java/ir/mahdiparastesh/sexbook/stat/Popularity.kt
@@ -1,18 +1,16 @@
 package ir.mahdiparastesh.sexbook.stat
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import com.anychart.AnyChart
-import com.anychart.chart.common.dataentry.DataEntry
-import com.anychart.chart.common.dataentry.ValueDataEntry
-import com.anychart.data.Set
-import com.anychart.enums.*
-import com.anychart.graphics.vector.Stroke
 import ir.mahdiparastesh.sexbook.Fun
 import ir.mahdiparastesh.sexbook.Model
 import ir.mahdiparastesh.sexbook.R
 import ir.mahdiparastesh.sexbook.databinding.PopularityBinding
+import lecho.lib.hellocharts.model.Line
+import lecho.lib.hellocharts.model.LineChartData
+import lecho.lib.hellocharts.model.PointValue
 
 class Popularity : AppCompatActivity() {
     private lateinit var b: PopularityBinding
@@ -32,73 +30,40 @@ class Popularity : AppCompatActivity() {
         var stars = ArrayList<Star>()
         for (x in m.summary.value!!.scores) {
             if (Summary.isUnknown(x.key)) continue
-            val scores = ArrayList<Float>()
+            val scores = ArrayList<Star.Frame>()
             for (month in stb)
-                scores.add(Singular.calcHistory(x.value, month))
+                scores.add(Star.Frame(Singular.calcHistory(x.value, month), month))
             stars.add(Star(x.key, scores.toTypedArray()))
         }
         stars.sortWith(Star.Sort(1))
         stars.sortWith(Star.Sort())
 
-        val data = ArrayList<DataEntry>()
-        for (s in stb.indices) {
-            val thisMonthsScores = ArrayList<Float>()
-            stars.forEach { thisMonthsScores.add(it.scores[s]) }
-            data.add(Frame(stb[s], thisMonthsScores))
-        }
-
-        AnyChart.line().apply {
-            animation(true)
-            crosshair().enabled(true)
-            crosshair()
-                .yLabel(true) // DO: yStroke
-                .yStroke(null as Stroke?, null, null, null as String?, null as String?)
-            tooltip().positionMode(TooltipPositionMode.POINT)
-
-            val set = Set.instantiate().apply { data(data) }
-            var ss = 0
-            stars.forEach {
-                line(
-                    set.mapAs("{ x: 'x', value: 'value${if (ss > 0) "$ss" else ""}' }")
-                ).name(it.name)
-                hovered().markers().apply {
-                    enabled(true)
-                    type(MarkerType.CIRCLE)
-                    size(4.0)
-                }
-                tooltip().apply {
-                    position("right")
-                    anchor(Anchor.LEFT_CENTER)
-                    offsetX(5.0)
-                    offsetY(5.0)
-                }
-                ss++
-            }
-            legend().apply { // Names of the Stars
-                enabled(true)
-                fontSize(13.0)
-                padding(0.0, 0.0, 11.0, 0.0)
-                position(Orientation.TOP)
-                positionMode(LegendPositionMode.OUTSIDE)
-            }
-            background(resources.getString(R.string.anyChartBG))
-            b.main.setChart(this)
-        }
+        b.main.lineChartData = LineChartData().setLines(LineFactory(stars))
     }
 
-    class Frame constructor(x: String, values: List<Number>) :
-        ValueDataEntry(x, values[0]) {
-        init {
-            for (i in 1 until values.size) setValue("value$i", values[i])
-        }
-    }
-
-    class Star(val name: String, val scores: Array<Float>) {
+    class Star(val name: String, val frames: Array<Frame>) {
         class Sort(val by: Int = 0) : Comparator<Star> {
             override fun compare(a: Star, b: Star) = when (by) {
                 1 -> a.name.compareTo(b.name)
-                else -> (b.scores.sum() - a.scores.sum()).toInt()
+                else -> b.frames.sumOf { (it.score * 100f).toInt() } -
+                        a.frames.sumOf { (it.score * 100f).toInt() }
             }
         }
+
+        data class Frame(val score: Float, val month: String)
     }
+
+    class LineFactory(stars: List<Star>) : ArrayList<Line>(stars.map {
+        Line(it.frames.mapIndexed { i, frame ->
+            PointValue(i.toFloat(), frame.score)
+                .setLabel("${it.name} : ${frame.month}")
+        })
+            .setColor(
+                arrayListOf(
+                    Color.BLUE, Color.RED, Color.CYAN, Color.GREEN, Color.BLACK
+                ).random()
+            )
+            .setCubic(true)
+            .setHasLabelsOnlyForSelected(true)
+    })
 }
