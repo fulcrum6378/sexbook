36fe03c431b5e792ee7139aa09e0ee330ea2b174
Mahdi Parastesh <fulcrum1378@gmail.com>
2022-12-29 07:53:35 +0330

    2022.12.29 - 2 (SumChips was migrated too, Recency doesn't work and both totally suck)

--- app/src/main/java/ir/mahdiparastesh/sexbook/list/StatSumAdap.kt
+++ app/src/main/java/ir/mahdiparastesh/sexbook/list/StatSumAdap.kt
@@ -1,4 +1,54 @@
 package ir.mahdiparastesh.sexbook.list
 
-class StatSumAdap {
+import android.content.Intent
+import android.view.ContextThemeWrapper
+import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.res.ResourcesCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+import ir.mahdiparastesh.sexbook.R
+import ir.mahdiparastesh.sexbook.databinding.SumChipGroupBinding
+import ir.mahdiparastesh.sexbook.more.AnyViewHolder
+import ir.mahdiparastesh.sexbook.more.BaseActivity
+import ir.mahdiparastesh.sexbook.stat.Singular
+
+class StatSumAdap(
+    private val c: BaseActivity,
+    private val arr: List<MutableMap.MutableEntry<Float, ArrayList<String>>>
+) : RecyclerView.Adapter<AnyViewHolder<SumChipGroupBinding>>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup, viewType: Int
+    ): AnyViewHolder<SumChipGroupBinding> =
+        AnyViewHolder(SumChipGroupBinding.inflate(c.layoutInflater, parent, false))
+
+    override fun onBindViewHolder(h: AnyViewHolder<SumChipGroupBinding>, i: Int) {
+        h.b.count.text =
+            (if (arr[i].key % 1 > 0) arr[i].key.toString()
+            else arr[i].key.toInt().toString()).plus(": ")
+
+        while (h.b.root.childCount != 1) h.b.root.removeViewAt(h.b.root.childCount - 1)
+        for (crush in arr[i].value) h.b.root.addView(
+            Chip(ContextThemeWrapper(c.c, R.style.Theme_MaterialComponents)).apply {
+                layoutParams = ChipGroup.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+                text = crush
+                val bb = c.m.lookingFor
+                    ?.let { it != "" && text.toString().contains(it, true) } ?: false
+                chipBackgroundColor = AppCompatResources.getColorStateList(
+                    c, if (!bb) R.color.chip_normal else R.color.chip_search
+                )
+                setTextColor(c.color(if (!bb) R.color.chipText else R.color.chipTextSearch))
+                setOnClickListener {
+                    c.m.crush = crush
+                    c.startActivity(Intent(c, Singular::class.java))
+                }
+                typeface = ResourcesCompat.getFont(c, R.font.normal)!!
+            })
+    }
+
+    override fun getItemCount() = arr.size
 }
--- app/src/main/java/ir/mahdiparastesh/sexbook/stat/SumChips.kt
+++ app/src/main/java/ir/mahdiparastesh/sexbook/stat/SumChips.kt
@@ -1,24 +1,20 @@
 package ir.mahdiparastesh.sexbook.stat
 
+import android.annotation.SuppressLint
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.view.ContextThemeWrapper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
-import androidx.appcompat.content.res.AppCompatResources
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.content.res.ResourcesCompat
-import androidx.core.view.get
 import androidx.fragment.app.Fragment
-import com.google.android.material.chip.Chip
-import com.google.android.material.chip.ChipGroup
 import ir.mahdiparastesh.sexbook.R
 import ir.mahdiparastesh.sexbook.databinding.SearchableStatBinding
+import ir.mahdiparastesh.sexbook.list.StatSumAdap
 import ir.mahdiparastesh.sexbook.more.BaseActivity
 
 class SumChips : Fragment() {
@@ -28,6 +24,7 @@ class SumChips : Fragment() {
     override fun onCreateView(inf: LayoutInflater, parent: ViewGroup?, state: Bundle?): View =
         SearchableStatBinding.inflate(inf, parent, false).apply { b = this }.root
 
+    @SuppressLint("NotifyDataSetChanged")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -35,57 +32,24 @@ class SumChips : Fragment() {
             override fun beforeTextChanged(s: CharSequence?, r: Int, c: Int, a: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
             override fun afterTextChanged(s: Editable?) {
-                val ss = s.toString()
-                for (i in 0 until b.ll.childCount) if (b.ll[i] is ChipGroup) {
-                    val cg = b.ll[i] as ChipGroup
-                    for (y in 1 until cg.childCount) (cg[y] as Chip).updateSearch(ss)
-                }
-                c.m.lookingFor = ss
+                c.m.lookingFor = s.toString()
+                b.list.adapter?.notifyDataSetChanged()
             }
         })
-        val lookingFor = c.m.lookingFor
-        lookingFor?.also { b.find.setText(it) }
+        c.m.lookingFor?.also { b.find.setText(it) }
 
         if (c.m.summary == null) return
-        for (r in c.m.summary!!.results().calculations) b.ll.addView(
-            ChipGroup(b.ll.context).apply {
-                layoutParams = LinearLayout.LayoutParams(
-                    ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
-                )
-                addView(AppCompatTextView(context).apply {
-                    layoutParams = ChipGroup.LayoutParams(
-                        ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
-                    )
-                    setPadding(0, c.dp(12), 0, 0)
-                    text = (if (r.key % 1 > 0) r.key.toString()
-                    else r.key.toInt().toString()).plus(": ")
-                    textSize = c.dm.density * 5
-                    typeface = ResourcesCompat.getFont(c, R.font.normal)!!
-                })
-                for (crush in r.value) addView(
-                    Chip(ContextThemeWrapper(c.c, R.style.Theme_MaterialComponents)).apply {
-                        layoutParams = ChipGroup.LayoutParams(
-                            ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
-                        )
-                        text = crush
-                        updateSearch(lookingFor)
-                        setOnClickListener {
-                            c.m.crush = crush
-                            startActivity(Intent(c, Singular::class.java))
-                        }
-                        typeface = ResourcesCompat.getFont(c, R.font.normal)!!
-                    })
-            })
-        c.m.summary?.nExcluded?.also {
+        b.list.adapter = StatSumAdap(c, c.m.summary!!.results().calculations.entries.toList())
+        /*FIXME c.m.summary?.nExcluded?.also {
             if (it > 0f) b.ll.addView(
                 plus(b.ll.context, getString(R.string.excStat, it.toString()))
             )
         }
-        c.m.summary?.unknown?.also {
+        FIXME c.m.summary?.unknown?.also {
             if (it > 0f) b.ll.addView(
                 plus(b.ll.context, getString(R.string.unknown, it.toString()))
             )
-        }
+        }*/
     }
 
     fun plus(c: Context, s: String) = AppCompatTextView(c).apply {
@@ -96,13 +60,5 @@ class SumChips : Fragment() {
         text = s
         alpha = .8f
         typeface = ResourcesCompat.getFont(c, R.font.normal)!!
-    }
-
-    private fun Chip.updateSearch(ss: String?) {
-        val bb = ss != null && ss != "" && text.toString().contains(ss, true)
-        chipBackgroundColor = AppCompatResources.getColorStateList(
-            c, if (!bb) R.color.chip_normal else R.color.chip_search
-        )
-        setTextColor(c.color(if (!bb) R.color.chipText else R.color.chipTextSearch))
     }
 }
